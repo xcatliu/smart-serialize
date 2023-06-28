aa84bc83f6f1a21a5f4c784edbfc3f36069398a7
xcatliu <xcatliu@gmail.com>
2023-06-28 10:15:32 +0800

    feat: 添加 serialize.js

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # smart-serialize
+
 Serialize any object, stringify, print to console, and write to clipboard
+
+当我们想要研究某个网站，找到了 window 上的一个变量，此时想要查看它的全貌，直接使用 `JSON.stringify` 是不行的，因为它大概率包含一些循环引用，还有很多无法序列化的内容，此时可以用这个脚本过滤掉循环引用和一些无法序列化的内容。
+
+## 使用方法
+
+将代码 [serialize.js](./serialize.js) 直接复制到浏览器控制台中使用。
+
+```js
+serialize(window.someVariable, options);
+```
+
+## 参数
+
+参数 | 描述 | 类型 | 默认值
+--- | --- | --- | ---
+`space` | 缩进空格数，`0` 则会输出为单行字符串 | `number` | `0`
+`useCircularPath` | 遇到循环引用时，是否输出引用路径。选是会将循环引用转为路径字符串 `$root.foo.bar`，选否则会转为 `$circular` | `boolean` | `true`
+`removeFunction` | 是否删除所有函数 | `boolean` | `false`
+`removeCircular` | 是否删除循环引用 | `boolean` | `false`
+`removeNull` | 是否删除 `null` | `boolean` | `false`
+`removeUndefined` | 是否删除 `undefined` | `boolean` | `false`
+`removeEmpty` | 是否删除空对象和空数组 | `boolean` | `false`
+`removeKeyFilter` | 删除符合条件的 key | `function` | `undefined`
+`removePathFilter` | 删除符合条件的 path | `function` | `undefined`
+`printToConsole` | 是否打印到控制台 | `boolean` | `true`
+`copyToClipboard` | 是否复制到粘贴板 | `boolean` | `true`
---
+++ serialize.js
@@ -0,0 +1,135 @@
+/**
+ * 序列化任意一个对象
+ */
+function serialize(obj, options = {}) {
+  const {
+    space,
+    useCircularPath,
+    removeFunction,
+    removeCircular,
+    removeNull,
+    removeUndefined,
+    removeEmpty,
+    removeKeyFilter,
+    removePathFilter,
+    printToConsole,
+    copyToClipboard,
+  } = {
+    space: 0,
+    useCircularPath: true,
+    removeFunction: false,
+    removeCircular: false,
+    removeNull: false,
+    removeUndefined: false,
+    removeEmpty: false,
+    removeKeyFilter: undefined,
+    removePathFilter: undefined,
+    printToConsole: true,
+    copyToClipboard: true,
+    ...options,
+  };
+
+  /** 换成对象的路径，方便后续检查是否存在循环引用 */
+  const objMap = new Map();
+  /**
+   * 将一个对象转换成可以安全 stringify 的对象
+   */
+  function safe(obj, pathPrefix = "$root") {
+    if (obj === null) {
+      if (removeNull) {
+        return "$remove";
+      }
+      return null;
+    }
+    if (obj === undefined) {
+      if (removeUndefined) {
+        return "$remove";
+      }
+      return undefined;
+    }
+    if (typeof obj === "function") {
+      if (removeFunction) {
+        return "$remove";
+      }
+      return "$function";
+    }
+    // 产生了循环引用
+    if (objMap.has(obj)) {
+      if (removeCircular) {
+        return "$remove";
+      }
+      if (useCircularPath) {
+        return objMap.get(obj);
+      }
+      return "$circular";
+    }
+    // 如果是对象或数组，则缓存到 objMap 中，方便后续检查是否存在循环引用
+    if (Array.isArray(obj)) {
+      objMap.set(obj, pathPrefix);
+      let result = [];
+      obj.forEach((value, index) => {
+        const newValue = safe(value, `${pathPrefix}[${index}]`);
+        if (newValue === "$remove") {
+          // should be removed
+        } else {
+          result.push(newValue);
+        }
+      });
+      if (removeEmpty && result.length === 0) {
+        return "$remove";
+      }
+      return result;
+    }
+    if (typeof obj === "object") {
+      objMap.set(obj, pathPrefix);
+      let result = {};
+      Object.entries(obj).forEach(([key, value]) => {
+        const newValue = safe(value, `${pathPrefix}.${key}`);
+        if (newValue === "$remove") {
+          // should be removed
+        } else {
+          result[key] = newValue;
+        }
+      });
+      if (removeEmpty && Object.keys(result).length === 0) {
+        return "$remove";
+      }
+      return result;
+    }
+    if (removeKeyFilter && !pathPrefix.endsWith(']') && removeKeyFilter(pathPrefix.match(/[^.]+$/)?.[0])) {
+      return "$remove";
+    }
+    if (removePathFilter && removePathFilter(pathPrefix)) {
+      return "$remove";
+    }
+    // 其他情况直接返回
+    return obj;
+  }
+
+  let safeObj = safe(obj);
+
+  let stringResult = "";
+  if (space === 0) {
+    stringResult = JSON.stringify(safeObj);
+  } else {
+    stringResult = JSON.stringify(safeObj, null, space);
+  }
+
+  if (printToConsole) {
+    console.log(stringResult);
+  }
+  if (copyToClipboard) {
+    copy(stringResult);
+  }
+
+  return stringResult;
+}
+
+function copy(text) {
+  const textarea = document.createElement("textarea");
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  document.body.removeChild(textarea);
+}
